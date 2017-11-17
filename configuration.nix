42be580e4e2998186c5f921eac416bf024e6df21
fusion809 <brentonhorne77@gmail.com>
2017-11-18 08:16:55 +1000

    Adding nix files

---
+++ configuration.nix
@@ -0,0 +1,107 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Use the GRUB 2 boot loader.
+  #boot.initrd.prepend = [ "/boot/initramfs-linux.img" ];
+  boot.extraGrubEntriesBeforeNixOS = true;
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+  boot.loader.grub.efiSupport = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  # boot.loader.grub.efiInstallAsRemovable = true;
+  # Define on which hard drive you want to install Grub.
+  boot.loader.grub.device = "nodev"; # or "nodev" for efi only
+  boot.loader.grub.useOSProber = true; # got to OS probe other distros
+
+  networking.hostName = "fusion809-pc"; # Define your hostname.
+  #networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Select internationalisation properties.
+  i18n = {
+    consoleFont = "Lat2-Terminus16";
+    consoleKeyMap = "us";
+    defaultLocale = "en_AU.UTF-8";
+  };
+
+  # Set your time zone.
+  time.timeZone = "Australia/Brisbane";
+
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  environment.systemPackages = with pkgs; [
+    wget vim git zsh firefox hexchat os-prober
+  ];
+
+  nixpkgs.config.allowUnfree = true;
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.bash.enableCompletion = true;
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+  #networking.networkmanager.enable = false;
+  users.extraUsers.fusion809 = 
+ { isNormalUser = true;
+   home = "/home/fusion809";
+   description = "Brenton's user";
+   extraGroups = [ "wheel" "networkmanager" ];
+ };
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+  services.xserver.layout = "us";
+  services.xserver.xkbOptions = "eurosign:e";
+
+  # Enable touchpad support.
+  services.xserver.libinput.enable = true;
+
+  # Enable the KDE Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome3.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  # users.extraUsers.guest = {
+  #   isNormalUser = true;
+  #   uid = 1000;
+  # };
+
+  # This value determines the NixOS release with which your system is to be
+  # compatible, in order to avoid breaking some software such as database
+  # servers. You should change this only after NixOS release notes say you
+  # should.
+  system.stateVersion = "17.09"; # Did you read the comment?
+  security.sudo.wheelNeedsPassword = false;
+
+  fileSystems.gentoo.device = "/dev/sda2";
+  fileSystems.gentoo.mountPoint = "/gentoo";
+  fileSystems.arch.device = "/dev/sda3";
+  fileSystems.arch.mountPoint = "/arch";
+  fileSystems.data.device = "/dev/sdb1";
+  fileSystems.data.mountPoint = "/data";
+  fileSystems.boot.device = "/dev/sda1";
+  fileSystems.boot.mountPoint = "/boot";
+}
+
