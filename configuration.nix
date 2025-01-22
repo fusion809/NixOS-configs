88673fbaa7278db8e6ce32caa94c473d9b37ce9a
Brenton Horne <brentonhorne77@gmail.com>
2025-01-22 13:24:16 +1000

    Copying from VM

--- configuration.nix
+++ configuration.nix
@@ -3,243 +3,235 @@
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
 { config, pkgs, ... }:
-
 {
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
     ];
 
-  # Use the GRUB 2 boot loader.
-  #boot.initrd.prepend = [ "/boot/initramfs-linux.img" ];
-  boot.loader.grub.default = 0;
+  # Bootloader.
   boot.loader.grub.enable = true;
-  boot.loader.grub.version = 2;
-  boot.loader.grub.efiSupport = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  # boot.loader.grub.efiInstallAsRemovable = true;
-  # Define on which hard drive you want to install Grub.
-  boot.loader.grub.device = "nodev"; # or "nodev" for efi only
-  boot.loader.grub.useOSProber = true; # got to OS probe other distros
-  boot.loader.grub.configurationLimit = 2;
-  nix.autoOptimiseStore = true;
-  #nixpkgs.config.firefox.enableAdobeFlash = true;
-  
-  networking.hostName = "fusion809-pc"; # Define your hostname.
-  # Select internationalisation properties.
-  console.keyMap = "us";
-  console.font = "Lat2-Terminus16";
-  i18n = {
-    defaultLocale = "en_AU.UTF-8";
-  };
+  boot.loader.grub.device = "/dev/sda";
+  boot.loader.grub.useOSProber = true;
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
 
   # Set your time zone.
   time.timeZone = "Australia/Brisbane";
 
-  # List packages installed in system profile. To search by name, run:
-  # $ nix-env -qaP | grep wget
-  nixpkgs.overlays = import /home/fusion809/.config/nixpkgs/overlays.nix ;
-  environment.systemPackages = with pkgs; [
-    wget psmisc efibootmgr xclip neofetch pciutils
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_GB.UTF-8";
 
-    papirus-icon-theme
-    tango-icon-theme
-    numix-icon-theme
-    paper-icon-theme
-    moka-icon-theme
-    arc-icon-theme
-    deepin.deepin-icon-theme
-    deepin.deepin-gtk-theme
-    adapta-gtk-theme
-    mojave-gtk-theme
-    numix-gtk-theme
-    numix-solarized-gtk-theme
-    numix-sx-gtk-theme
-    paper-gtk-theme
-    sierra-gtk-theme
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_AU.UTF-8";
+    LC_IDENTIFICATION = "en_AU.UTF-8";
+    LC_MEASUREMENT = "en_AU.UTF-8";
+    LC_MONETARY = "en_AU.UTF-8";
+    LC_NAME = "en_AU.UTF-8";
+    LC_NUMERIC = "en_AU.UTF-8";
+    LC_PAPER = "en_AU.UTF-8";
+    LC_TELEPHONE = "en_AU.UTF-8";
+    LC_TIME = "en_AU.UTF-8";
+  };
 
-    # Archiving
-    p7zip
-    ark
-    unzip
-    libarchive
-
-    # i3 stuff
-    dolphin
-    pcmanfm
-    lxappearance
-
-    # Audio/graphics/video/fonts
-    ffmpegthumbnailer
-    ffmpeg
-    ffmpegthumbs
-    kdeApplications.kio-extras
-    font-awesome_5
-    gimp
-    optipng
-    imagemagick
-    inkscape
-    vlc
-    mediainfo-gui
-    librsvg
-    mate.eom
-
-    # Networking
-    google-chrome
-    aria2
-    thunderbird
-
-    # Social
-    zoom-us
-    audacity
-    skypeforlinux
-    hexchat
-    gnome3.cheese
-    gnome3.gnome-tweak-tool
-
-    # Office
-    libreoffice-fresh
-    zotero
-    gnome3.evince
-
-    # Editors
-    atom
-    vscode
-    notepadqq
-    vimHugeX
-  
-    # Development tools
-    gist
-    git
-    gitAndTools.hub
-    
-    # Chemistry
-    avogadro
-    marvin
-    jmol
-
-    # Maths
-    jabref
-    ( octaveFull.override { python = python.withPackages (ps: with ps; [ sympy ]);} ) 
-    texlive.combined.scheme-full
-    texstudio
-
-    # Games
-    superTuxKart
-    superTux
-    gnome3.aisleriot
-    gnome3.gnome-chess
-    gnome3.gnome-mines
-    gnome3.gnome-mahjongg
-
-    # Zenity
-    gnome3.zenity
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+  services.xserver.excludePackages = with pkgs; [
+	xterm
   ];
 
-  # As user I could install additional packages, like: 
-  # atom, codeblocks-full, julia_10, octaveFull, #openra, sage, scilab-bin, vscode
-  services.flatpak.enable = true;
-  services.accounts-daemon.enable = true;
-  environment.shells = [
-     pkgs.zsh pkgs.bashInteractive
-  ];
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.displayManager.gdm.wayland = false; # Make it use X11 by default
+  services.gnome.gnome-browser-connector.enable = true;
+  #nixpkgs.config.permittedInsecurePackages = [
+  #  "dotnet-sdk-6.0.428"
+  #];
+  environment.gnome.excludePackages = (with pkgs; [
+  	epiphany
+        geary
+	totem
+	gnome-calculator
+	gnome-calendar
+	gnome-clocks
+	gnome-contacts
+	gnome-disk-utility
+	gnome-music
+	gnome-system-monitor
+	gnome-tour
+	gnome-weather
+	evince
+	gnome-connections
+	snapshot
+	gnome-characters
+	baobab
+	loupe
+	simple-scan
+	file-roller
+	rygel
+	gnome-bluetooth
+	gnome-color-manager
+	gnome-console
+	gnome-font-viewer
+	gnome-online-accounts
+	gnome-remote-desktop
+	gnome-user-share
+	seahorse
+  ]);
 
-  users.defaultUserShell = pkgs.zsh;
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = false;
+
+  # Enable sound with pipewire.
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  security.sudo.wheelNeedsPassword = false;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.fusion809 = {
+    isNormalUser = true;
+    description = "Brenton Horne";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+    #  thunderbird
+    ];
+  };
+
+  # Enable automatic login for the user.
+  services.displayManager.autoLogin.enable = true;
+  services.displayManager.autoLogin.user = "fusion809";
+
+  # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
+  systemd.services."getty@tty1".enable = false;
+  systemd.services."autovt@tty1".enable = false;
+
+  # Install firefox.
+  programs.firefox.enable = true;
+
+  # Omit nano
+  programs.nano.enable = false;
+
+  # Use Vim instead
+  programs.vim.enable = true;
+  programs.vim.defaultEditor = true;
+  programs.vim.package = pkgs.vim-full;
+
+  # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
- 
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  nixpkgs.overlays = import /home/fusion809/overlays.nix;
+
+  environment.systemPackages = (with pkgs; [
+	fastfetch
+	hyfetch
+        wget
+	gnome-tweaks
+        whitesur-gtk-theme
+        whitesur-cursors
+        whitesur-icon-theme
+        gnome-terminal
+	deepin.deepin-wallpapers
+        budgie-backgrounds
+	pantheon.elementary-wallpapers
+        unzip
+	cosmic-wallpapers
+	git
+	xclip
+    ]) ++ (with pkgs.nixos-artwork.wallpapers; [
+		binary-black
+		catppuccin-mocha
+		catppuccin-macchiato
+		catppuccin-latte
+		catppuccin-frappe
+		moonscape
+		nineish
+		nineish-dark-gray
+		nineish-solarized-dark
+		nineish-solarized-light
+		simple-blue
+		simple-dark-gray
+		simple-dark-gray-bootloader
+		simple-dark-gray-bottom
+		simple-light-gray
+		simple-red
+		stripes-logo
+		stripes
+		waterfall
+		watersplash
+	]);
+
+  environment.pathsToLink = ["/share/backgrounds/nixos"];
+  programs.zsh.enable = true;
+  programs.zsh.ohMyZsh.enable = true;
+  programs.zsh.autosuggestions.enable = true;
+  programs.zsh.syntaxHighlighting.enable = true;
+  programs.zsh.enableCompletion = true;
+  programs.zsh.ohMyZsh.plugins = [
+	"safe-paste"
+	"vi-mode"
+  ];
+  users.defaultUserShell = pkgs.zsh;
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
-  # programs.bash.enableCompletion = true;
   # programs.mtr.enable = true;
-  # programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
 
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-  sound.enable = true;
-  hardware.pulseaudio.enable = true;
-  hardware.pulseaudio.support32Bit = true;
+  # services.openssh.enable = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
-  networking.networkmanager.enable = true;
-  users.extraUsers.fusion809 = 
- { isNormalUser = true;
-   home = "/home/fusion809";
-   uid = 1000;
-   description = "Brenton Horne";
-   extraGroups = [ "audio" "wheel" "networkmanager" ];
-   packages = [
-   (pkgs.appimage-run.override {
-      extraPkgs = p: with p; [ harfbuzz mono fuse ]; 
-    })
-    ];
- };
 
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.11"; # Did you read the comment?
+  virtualisation.virtualbox.guest.enable = true;
+  virtualisation.virtualbox.guest.dragAndDrop = true;
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-  services.xserver.layout = "us";
-  services.xserver.xkbOptions = "eurosign:e";
-
-  # Enable touchpad support.
-  services.xserver.libinput.enable = true;
-
-  services.xserver.videoDrivers = [ "nvidia" ];
-
-  # Enable the KDE Desktop Environment.
-  #services.xserver.desktopManager.plasma5.enable = true;
-  # Enable the GNOME Desktop Environment.
-  #services.xserver.desktopManager.gnome3.enable = true;
-  #services.gnome3.chrome-gnome-shell.enable = true;
-  # i3
-  services.xserver.windowManager.i3.enable = true;
-  services.xserver.windowManager.i3.extraPackages = with pkgs; [
-     rofi i3status konsole playerctl
-     (python37.withPackages(ps: [ (ps.toPythonModule (i3pystatus.override { python3Packages = ps; })) ]))
-  ];
-  services.xserver.displayManager.sddm = {
-     enable = true;
-     autoLogin.enable = true;
-     autoLogin.user = "fusion809";
-  };
-  services.xserver.displayManager.gdm = {
-     enable = false;
-  };
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  # users.extraUsers.guest = {
-  #   isNormalUser = true;
-  #   uid = 1000;
-  # };
-
-  # This value determines the NixOS release with which your system is to be
-  # compatible, in order to avoid breaking some software such as database
-  # servers. You should change this only after NixOS release notes say you
-  # should.
-  system.autoUpgrade.enable = true;
-  system.autoUpgrade.channel = https://nixos.org/channels/nixos-unstable;
-  system.autoUpgrade.dates = "19:30";
-  system.stateVersion = "20.03pre"; # Did you read the comment?
-  security.sudo.enable = true;
-  security.sudo.wheelNeedsPassword = false;
-
-  # Using UUIDs is important, as if /dev/sdXn is used NixOS will try to mount the wrong petition
-  # Due to the presence of two disks, which confuses it sometimes
-  fileSystems.arch.device = "/dev/disk/by-uuid/c324ba12-1932-45e4-9f38-f13bbcfef1d0";
-  fileSystems.arch.mountPoint = "/arch";
-  fileSystems.data.device = "/dev/disk/by-uuid/95eac782-d467-42c1-b31e-11020b29a353";
-  fileSystems.data.mountPoint = "/data";
-  fileSystems.debian.device = "/dev/disk/by-uuid/dd381240-03fa-4255-a89b-39b6b28bd0a0";
-  fileSystems.debian.mountPoint = "/debian";
-  virtualisation.virtualbox.host.enable = true;
-  virtualisation.virtualbox.host.enableExtensionPack = true;
-  nixpkgs.config.allowBroken = true;
-  xdg.portal.enable = true;
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 }
+
