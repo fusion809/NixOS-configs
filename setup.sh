87a06e6a78617a7672fd3a75bfeeed8709051e10
Brenton Horne <brentonhorne77@gmail.com>
2025-01-22 19:50:42 +1000

    Tweaking setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,10 @@
 #!/run/current-system/sw/bin/zsh
-ssh-keygen -t rsa -b 4096 -C "brentonhorne77@gmail.com"
-git config --global user.name "fusion809"
+if ! [[ -f $HOME/.ssh/id_rsa ]]; then
+	ssh-keygen -t rsa -b 4096 -C "brentonhorne77@gmail.com"
+	clipf $HOME/.ssh/id_rsa.pub
+	printf "Go to https://github.com/settings/keys and create new SSH key.\n The contents are in your clipboard"
+fi
+git config --global user.name "Brenton Horne"
 git config --global user.email "brentonhorne77@gmail.com"
-clipf $HOME/.ssh/id_rsa.pub
-printf "Go to https://github.com/settings/keys and create new SSH key.\n The contents are in your clipboard"
 ./setup-symlinks.sh
 
