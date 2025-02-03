1134a307e092a1c87391e43f88822ac4bf5e87b0
fusion809 <brentonhorne77@gmail.com>
2025-02-03 23:21:37 +1000

    Improving setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ parted $disk --script mklabel msdos   # Create a DOS (MBR) partition table
 parted $disk --script mkpart primary ext4 1MiB 100%  # Create a primary partition using the whole disk
 parted $disk --script set 1 boot on   # Set the partition as bootable
 root="${disk}1"
-root_label=$(cat hardware-configuration.nix | grep by-label | cut -d '/' -f 5)
+root_label=$(cat hardware-configuration.nix | grep by-label | cut -d '/' -f 5 | cut -d '"' -f 1)
 mkfs.ext4 -L $root_label $root
 mount $root /mnt
 mkdir -p /mnt/etc/nixos
@@ -15,3 +15,7 @@ nix-channel --add https://nixos.org/channels/nixos-${VERSION} nixos
 nix-channel --add https://github.com/nix-community/home-manager/archive/release-${VERSION}.tar.gz home-manager
 nix-channel --update
 nixos-install
+cp -r ../NixOS-configs /mnt/home/fusion809
+nixos-enter -c "chown fusion809 /home/fusion809/NixOS-configs; chown fusion809 -R /home/fusion809/NixOS-configs"
+cp -r .root-bashrc /mnt/root/.bashrc
+cp -r .root-zshrc /mnt/root/.zshrc
