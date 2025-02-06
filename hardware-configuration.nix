3f2da84f6549988ec858634830bee0532f8b706e
fusion809 <brentonhorne77@gmail.com>
2025-02-06 15:05:26 +1000

    Minor fixes to install

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -12,7 +12,7 @@
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-label/nixos-root";
+    { device = "/dev/disk/by-label/nixos";
       fsType = "ext4";
     };
 
--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ mkfs.ext4 -L $root_label $root
 mount $root /mnt
 mkdir -p /mnt/etc/nixos
 cp *.nix /mnt/etc/nixos
-VERSION=$(cat .git/HEAD | cut -d '/' -f 3)
+VERSION=$(cat .git/HEAD | cut -d '/' -f 3 | head -n 1)
 if [[ $VERSION == "unstable" ]]; then
 	HOMEVER="master"
 else
