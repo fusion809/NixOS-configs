6a96d401ea45d23a217cf8586dfb024e4a9f9fbf
fusion809 <brentonhorne77@gmail.com>
2025-05-25 13:32:58 +1000

    hardware.pulseaudio->services.pulseaudio

--- configuration.nix
+++ configuration.nix
@@ -125,7 +125,7 @@
   services.printing.enable = false;
 
   # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
+  services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   security.sudo.wheelNeedsPassword = false;
   services.pipewire = {
