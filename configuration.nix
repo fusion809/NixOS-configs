078e994153c149dc44592502b376186f72131ebb
installer <installer@gmail.com>
2025-10-10 23:16:46 +1000

    Switching to grub

--- configuration.nix
+++ configuration.nix
@@ -17,7 +17,10 @@ home-manager.users.fusion809 = {
           ];
   };
   # Bootloader.
-  boot.loader.systemd-boot.enable = true;
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "nodev";
+  boot.loader.grub.efiSupport = true;
+  boot.loader.grub.useOSProber = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
   # Use latest kernel.
