151f62f5ff5c50acbd755fcf6e50649d0ca8b3b0
installer <installer@gmail.com>
2025-10-27 14:06:51 +1000

    Adding flake and Hyprland

--- flake.nix
+++ flake.nix
@@ -1,13 +1,40 @@
+# flake.nix
+
 {
-  description = "Hyprland nix";
   inputs = {
-  # ...
-  hyprland.url = "github:hyprwm/Hyprland";
-  hyprland-plugins = {
-    url = "github:hyprwm/hyprland-plugins";
-    inputs.hyprland.follows = "hyprland";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1&ref={version}";
+    # where {version} is the hyprland release version
+    # or "github:hyprwm/Hyprland?submodules=1" to follow the development branch
+
+    hy3 = {
+      url = "github:outfoxxed/hy3?ref=hl{version}"; # where {version} is the hyprland release version
+      # or "github:outfoxxed/hy3" to follow the development branch.
+      # (you may encounter issues if you dont do the same for hyprland)
+      inputs.hyprland.follows = "hyprland";
+    };
   };
 
-  # ...
-};
+  outputs = { nixpkgs, home-manager, hyprland, hy3, ... }: {
+    homeConfigurations."user@hostname" = home-manager.lib.homeManagerConfiguration {
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+
+      modules = [
+        hyprland.homeManagerModules.default
+
+        {
+          wayland.windowManager.hyprland = {
+            enable = true;
+            plugins = [ hy3.packages.x86_64-linux.hy3 ];
+          };
+        }
+      ];
+    };
+  };
 }
