8c54b593aa98a7341d42e4ab97b6ff32f2f4ebff
Brenton Horne <brentonhorne77@gmail.com>
2025-11-01 22:32:44 +1000

    Restructuring home.nix and adding kitty lines *maybe* might fix issue with nerd fonts

--- home.nix
+++ home.nix
@@ -69,15 +69,11 @@
   };
 
   # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
-  programs.git = {
-    enable = true;
-    userName = "fusion809";
-    userEmail = "brentonhorne77@gmail.com";
-  };
-  programs.bash = {
-    enable = true;
-    bashrcExtra = "
+  programs = {
+  	home-manager.enable = true;
+  	bash = {
+    		enable = true;
+    		bashrcExtra = "
 function vbash {
   vim $HOME/.bashrc
 }
@@ -204,10 +200,50 @@ if ! `mountpoint -q /arch`; then
   sudo mount /dev/disk/by-label/ARCHEFI /arch/boot
 fi
   ";
-  };
-  programs.zsh = {
-    enable = true;
-    initExtra = "
+  	};
+  	git = {
+	    	enable = true;
+    		userName = "fusion809";
+   	  	userEmail = "brentonhorne77@gmail.com";
+  	};
+    kitty = lib.mkForce {
+      enable = true;
+      settings = {
+        confirm_os_window_close = 0;
+        dynamic_background_opacity = true;
+        enable_audio_bell = false;
+        mouse_hide_wait = "-1.0";
+        window_padding_width = 10;
+        background_opacity = "0.5";
+        background_blur = 5;
+        symbol_map = let
+        mappings = [
+          "U+23FB-U+23FE"
+          "U+2B58"
+          "U+E200-U+E2A9"
+          "U+E0A0-U+E0A3"
+          "U+E0B0-U+E0BF"
+          "U+E0C0-U+E0C8"
+          "U+E0CC-U+E0CF"
+          "U+E0D0-U+E0D2"
+          "U+E0D4"
+          "U+E700-U+E7C5"
+          "U+F000-U+F2E0"
+          "U+2665"
+          "U+26A1"
+          "U+F400-U+F4A8"
+          "U+F67C"
+          "U+E000-U+E00A"
+          "U+F300-U+F313"
+          "U+E5FA-U+E62B"
+        ];
+          in
+            (builtins.concatStringsSep "," mappings) + " Symbols Nerd Font";
+      };
+    };
+	  zsh = {
+		  enable = true;
+    	initExtra = "
 sed -i '/^:/!d' $HOME/.zsh_history
 source $HOME/NixOS-configs/hnixos.zsh-theme
 function shopt {
