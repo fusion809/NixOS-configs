a6513140e589dac5c69478d3e2e48bd1c9673752
fusion809 <brentonhorne77@gmail.com>
2025-11-02 03:20:11 +1000

    Committing attempted improvement to distrohoop flake.nix

--- flake.nix
+++ flake.nix
@@ -3,22 +3,45 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
-    distrohoop.url = "github:br0sinski/distrohoop";
-  };
-
-  outputs = { self, nixpkgs, distrohoop }: {
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ./configuration.nix
-        {
-          nixpkgs.overlays = [
-            (final: prev: {
-              distrohoop = distrohoop.packages.${prev.system}.default;
-            })
-          ];
-        }
-      ];
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    distrohoop = {
+      url = "github:br0sinski/distrohoop";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
+
+  outputs = { self, nixpkgs, home-manager, distrohoop }: 
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+    in {
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          ./configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            nixpkgs.overlays = [
+              (final: prev: {
+                distrohoop = distrohoop.packages.${system}.default;
+              })
+            ];
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              users.fusion809 = { ... }: {
+                home.stateVersion = "25.05";
+                home.packages = [ pkgs.distrohoop ];
+              };
+            };
+          }
+        ];
+      };
+    };
 }
