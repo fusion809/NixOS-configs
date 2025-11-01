b94dd0a0bcbefac6ba03044fd36baef7b99633e7
fusion809 <brentonhorne77@gmail.com>
2025-11-02 03:27:44 +1000

    Fixing flake; it is working now

--- flake.nix
+++ flake.nix
@@ -3,10 +3,6 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
-    home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     distrohoop = {
       url = "github:br0sinski/distrohoop";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -25,21 +21,12 @@
         inherit system;
         modules = [
           ./configuration.nix
-          home-manager.nixosModules.home-manager
           {
             nixpkgs.overlays = [
               (final: prev: {
                 distrohoop = distrohoop.packages.${system}.default;
               })
             ];
-            home-manager = {
-              useGlobalPkgs = true;
-              useUserPackages = true;
-              users.fusion809 = { ... }: {
-                home.stateVersion = "25.05";
-                home.packages = [ pkgs.distrohoop ];
-              };
-            };
           }
         ];
       };
