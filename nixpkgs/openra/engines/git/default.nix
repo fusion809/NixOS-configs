2f3fd7bf09083d17ae84f76f0f161aa40f72301f
fusion809 <brentonhorne77@gmail.com>
2025-11-03 04:06:27 +1000

    Fixing openra git vars, after latest update

--- nixpkgs/openra/engines/git/default.nix
+++ nixpkgs/openra/engines/git/default.nix
@@ -3,8 +3,8 @@
 buildOpenRAEngine {
   build = "git";
   version = "20250531";
-  version = "2cb1e5f8c546196e911827e5d33f3b686c3bf452";
-  sha256 = "sha256-cAUAVdcoiAGo9x9ADs/QmFQ1sviVTMiqJilPrRbTexI=";
+  rev = "2cb1e5f8c546196e911827e5d33f3b686c3bf452";
+  hash = "sha256-cAUAVdcoiAGo9x9ADs/QmFQ1sviVTMiqJilPrRbTexI=";
   deps = ./deps.json;
   dotnet-sdk = dotnetCorePackages.sdk_8_0-bin;
 }
