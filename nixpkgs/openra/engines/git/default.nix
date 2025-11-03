cb6dfa34c5af1c5c7b62224e7ea0846d04258ab6
fusion809 <brentonhorne77@gmail.com>
2025-11-04 09:29:06 +1000

    version: updating to 20251104

--- nixpkgs/openra/engines/git/default.nix
+++ nixpkgs/openra/engines/git/default.nix
@@ -2,7 +2,7 @@
 
 buildOpenRAEngine {
   build = "git";
-  version = "20250531";
+  version = "20251104";
   rev = "10db26fa0b3df25e679b465475fe69b21381b26b";
   hash = "sha256-8DZDjnviZyQ+9PuA0hyRn6tyyGqlqBX6C5GkOUjzidM=";
   deps = ./deps.json;
