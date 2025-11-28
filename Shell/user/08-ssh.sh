3a367189874181dd1298a052bc312ccbdcd899ea
fusion809 <brentonhorne77@gmail.com>
2025-11-28 23:45:40 +1000

    Adding ssh commands for QEMU

---
+++ Shell/user/08-ssh.sh
@@ -0,0 +1,22 @@
+if ! [[ -d $HOME/.ssh ]] || ! [[ -f $HOME/.ssh/id_rsa.pub ]]; then
+  mkdir -p $HOME/.ssh
+  ssh-keygen -t rsa -b 4096 -C 'brentonhorne77@gmail.com'
+  clipf $HOME/.ssh/id_rsa.pub
+  echo 'GitHub SSH key generated and is now in your clipboard. Go to https://github.com/settings/ssh to register it to your account!'
+fi
+
+function ssh_debian {
+    ssh fusion809@192.168.122.244
+}
+
+function cp_from_debian {
+    scp -O -r fusion809@192.168.122.244:/home/fusion809/$1 /arch$HOME/PhD/Rcode/
+}
+
+function ssh_fedora {
+    ssh fusion809@192.168.122.232
+}
+
+function ssh_ubuntu {
+    ssh fusion809@192.168.122.151
+}
