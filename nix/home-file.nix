25037bf7681019ad7cfa3aeee3dc9ad58dc5c5fd
fusion809 <brentonhorne77@gmail.com>
2025-11-30 17:28:23 +1000

    Reducing hardcoded paths in home-file.nix

--- nix/home-file.nix
+++ nix/home-file.nix
@@ -1,35 +1,35 @@
 { config, pkgs, ... }:
 
-{
+let dotfilesDir = /home/fusion809/GitHub/mine/config/NixOS-configs/dotfiles;
+in {
   ".local/share/hyprland/plugins/hy3.so".source =
     "${pkgs.hyprlandPlugins.hy3}/lib/libhy3.so";
 
   # Hyprland
-  ".config/hypr/hyprland.conf".source = config.lib.file.mkOutOfStoreSymlink
-    /home/fusion809/GitHub/mine/config/NixOS-configs/dotfiles/hyprland.conf;
+  ".config/hypr/hyprland.conf".source =
+    config.lib.file.mkOutOfStoreSymlink "${dotfilesDir}/hyprland.conf";
 
   # Waybar
-  ".config/waybar/config.jsonc".source = config.lib.file.mkOutOfStoreSymlink
-    /home/fusion809/GitHub/mine/config/NixOS-configs/dotfiles/waybar-config.jsonc;
-  ".config/waybar/style.css".source = config.lib.file.mkOutOfStoreSymlink
-    /home/fusion809/GitHub/mine/config/NixOS-configs/dotfiles/style.css;
-  ".config/waybar/nixos_menu.xml".source = config.lib.file.mkOutOfStoreSymlink
-    /home/fusion809/GitHub/mine/config/NixOS-configs/dotfiles/nixos_menu.xml;
+  ".config/waybar/config.jsonc".source =
+    config.lib.file.mkOutOfStoreSymlink "${dotfilesDir}/waybar-config.jsonc";
+  ".config/waybar/style.css".source =
+    config.lib.file.mkOutOfStoreSymlink "${dotfilesDir}/style.css";
+  ".config/waybar/nixos_menu.xml".source =
+    config.lib.file.mkOutOfStoreSymlink "${dotfilesDir}/nixos_menu.xml";
 
   # Kitty
-  ".config/kitty/kitty.conf".source = config.lib.file.mkOutOfStoreSymlink
-    /home/fusion809/GitHub/mine/config/NixOS-configs/dotfiles/kitty.conf;
+  ".config/kitty/kitty.conf".source =
+    config.lib.file.mkOutOfStoreSymlink "${dotfilesDir}/kitty.conf";
 
   # Alacritty
   ".config/alacritty/alacritty.toml".source =
-    config.lib.file.mkOutOfStoreSymlink
-    /home/fusion809/GitHub/mine/config/NixOS-configs/dotfiles/alacritty.toml;
+    config.lib.file.mkOutOfStoreSymlink "${dotfilesDir}/alacritty.toml";
 
   # Fastfetch
-  ".config/fastfetch/config.jsonc".source = config.lib.file.mkOutOfStoreSymlink
-    /home/fusion809/GitHub/mine/config/NixOS-configs/dotfiles/fastfetch-config.jsonc;
+  ".config/fastfetch/config.jsonc".source =
+    config.lib.file.mkOutOfStoreSymlink "${dotfilesDir}/fastfetch-config.jsonc";
 
   # SSH
-  ".ssh/config".source = config.lib.file.mkOutOfStoreSymlink
-    /home/fusion809/GitHub/mine/config/NixOS-configs/dotfiles/config;
+  ".ssh/config".source =
+    config.lib.file.mkOutOfStoreSymlink "${dotfilesDir}/config";
 }
