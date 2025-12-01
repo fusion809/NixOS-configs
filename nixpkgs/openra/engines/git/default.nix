a73e6d5bfb2055a87224864a3fcce7c489c84c2b
fusion809 <brentonhorne77@gmail.com>
2025-12-02 07:39:06 +1000

    Adjusting void IP

--- nixpkgs/openra/engines/git/default.nix
+++ nixpkgs/openra/engines/git/default.nix
@@ -2,9 +2,9 @@
 
 buildOpenRAEngine {
   build = "git";
-  version = "30617.git.7e8f4a3";
-  rev = "7e8f4a3479d704662a0f39a73476d2beb46004c1";
-  hash = "sha256-XifwpAhyWv04qyEMzkj2UsW37RWUUoJYq5U17hzKvPA=";
+  version = "30621.git.62e6920";
+  rev = "62e692063ad9d5a39bc1566308aecf89ce9bb95b";
+  hash = "sha256-ij0Qwt5fzI+hCv1EXAdQ5BTekv3GQ1/zcsoaZxyPyDo=";
   deps = ./deps.json;
   dotnet-sdk = dotnetCorePackages.sdk_8_0-bin;
 }
