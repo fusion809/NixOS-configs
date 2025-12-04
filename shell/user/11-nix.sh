267f963ccdc96cd77cf616dd4bc792669e5a0419
fusion809 <brentonhorne77@gmail.com>
2025-12-04 10:35:36 +1000

    nixfrb: adding push line

--- shell/user/11-nix.sh
+++ shell/user/11-nix.sh
@@ -20,6 +20,7 @@ function nixfrb {
     git add --all
   fi
   sudo nixos-rebuild switch -I nixos-config="$NIXCFG/nix/configuration.nix" --flake "$NIXCFG/nix/#nixos" --impure
+  push "Updating flake.lock"
 }
 
 function nixfu {
