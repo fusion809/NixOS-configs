c62ee94385a9658a677101429112915cbb7cfbd9
fusion809 <brentonhorne77@gmail.com>
2025-12-06 17:37:49 +1000

    Tweaking updates to only add letter/symbol indicators if they're missing

--- shell/user/11-nix.sh
+++ shell/user/11-nix.sh
@@ -18,7 +18,7 @@ function nixfrb {
     git add --all
   fi
   sudo nixos-rebuild switch -I nixos-config="$NIXCFG/nix/configuration.nix" --flake "$NIXCFG/nix/#nixos" --impure
-  echo "" > $HOME/.cache/update
+  sed -i -e "2s|.*||g" $HOME/.cache/update
 }
 
 function nixfu {
