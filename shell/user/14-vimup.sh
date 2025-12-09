94b2e4c33fbec35ba8a3c2dc46cda316a125e0c7
fusion809 <brentonhorne77@gmail.com>
2025-12-09 11:00:46 +1000

    Silencing vimup error

--- shell/user/14-vimup.sh
+++ shell/user/14-vimup.sh
@@ -3,13 +3,14 @@ if [[ $(which vim) != "vim not found" ]]; then
     vim_basever=$(echo $vim_verout | head -n 1 | cut -d ' ' -f 5)
     vim_patchver=$(echo $vim_verout | tail -n 1 | cut -d '-' -f 2)
     export vim_instver="$vim_basever.$vim_patchver"
-else
-    export vim_instver=""
 fi
+
 export vim_upver=$(wget -q https://github.com/vim/vim/tags -O - | grep "tar\.gz" | head -n 1 | cut -d '/' -f 7 | cut -d '"' -f 1 | sed 's/v//g' | sed 's/\.tar\.gz//g')
 
 if ( [[ $vim_instver == $vim_upver ]] || [[ -z $vim_upver ]] ) && [[ $(which vim) != "vim not found" ]]; then
     return 1
 fi
 
-echo "You should run nixrsu to update your system, as Vim $vim_upver is out and $vim_instver is installed."
+if [[ -z $vim_instver ]]; then
+    echo "You should run nixrsu to update your system, as Vim $vim_upver is out and $vim_instver is installed."
+fi
