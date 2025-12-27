ebabd24eca3470d228ad95aa9e96083e226c9fa4
fusion809 <brentonhorne77@gmail.com>
2025-12-28 00:21:04 +1000

    updates: Updating flake.lock

--- nix/packages.nix
+++ nix/packages.nix
@@ -138,6 +138,7 @@ with pkgs; [
   docker
   docker-compose
   freerdp
+  OVMF
   xorriso # Can be used to get files from host to guest
   virt-viewer
   #(unstable.winboat.override { nodejs_24 = pkgs.nodejs_24; })
