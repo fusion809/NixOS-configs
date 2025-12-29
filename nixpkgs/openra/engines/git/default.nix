b1ef80b4d28a55d0de343302262882e3e723a551
fusion809 <brentonhorne77@gmail.com>
2025-12-29 22:02:52 +1000

    updates: Updating flake.lock; openra: 30676.git.fdb4ce4->30677.git.2246922

--- nixpkgs/openra/engines/git/default.nix
+++ nixpkgs/openra/engines/git/default.nix
@@ -17,7 +17,7 @@ in buildOpenRAEngine {
   build = "git";
   inherit version;
   rev = openraSrc.rev;
-  hash = "sha256-b8dYzTGOP3xfbscE5xTsmxDmLFBXcKcBcrDIMKaL3Fc=";
+  hash = "sha256-InFr6jhobYIqa168OXeI7Dljtf7PDFFXiu8/mReGBg0=";
   src = openraSrc;
   deps = ./deps.json;
   dotnet-sdk = dotnetCorePackages.sdk_8_0-bin;
