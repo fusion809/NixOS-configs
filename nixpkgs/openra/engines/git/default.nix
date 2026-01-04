07aa2d74ea0152f3c5218dcad33efd5b345e24a0
fusion809 <brentonhorne77@gmail.com>
2026-01-04 22:42:14 +1000

    openra: 30705.git.c47ebfb->30708.git.032f20b

--- nixpkgs/openra/engines/git/default.nix
+++ nixpkgs/openra/engines/git/default.nix
@@ -17,7 +17,7 @@ in buildOpenRAEngine {
   build = "git";
   inherit version;
   rev = openraSrc.rev;
-  hash = "sha256-2fB94UzAKrdunITsNL5gFXPg4XD30chWIJ+Y43AL6zg=";
+  hash = "sha256-e15z5GIXDHWW4/dEgw7OJJTIU4SqMzg0iGAVfk1dwU0=";
   src = openraSrc;
   deps = ./deps.json;
   dotnet-sdk = dotnetCorePackages.sdk_8_0-bin;
