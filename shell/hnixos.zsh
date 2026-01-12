2c06163802af5a0cd181969f315ab0d660dc99a8
fusion809 <brentonhorne77@gmail.com>
2026-01-12 15:18:27 +1000

    Rm space before $/# in prompt

--- shell/hnixos.zsh
+++ shell/hnixos.zsh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 function prompt_char {
-	if [ $UID -eq 0 ]; then echo "%{$fg_bold[red]%}"; else echo "%{$fg_bold[green]%}$"; fi
+	if [ $UID -eq 0 ]; then echo "%{$fg_bold[red]%}#"; else echo "%{$fg_bold[green]%}$"; fi
 }
 
 function operating_system {
@@ -12,7 +12,7 @@ function operating_system {
 
 function user {
 	if id | grep root > /dev/null 2>&1; then
-		echo "%{$fg_bold[red]%}$(printf '%-6s' "root")%{$reset_color%}"
+		echo "%{$fg_bold[red]%}root%{$reset_color%}"
 	else
 		echo "%{$fg_bold[green]%}$(printf '%-6s' "${USER}")%{$reset_color%}"
 	fi
@@ -25,18 +25,12 @@ function conditional_newline {
 	local prompt_len=${#clean_prompt}
 	if (( prompt_len > COLUMNS * 0.9 )); then
 		echo $'\n'
-	else
-		echo " "
 	fi
 }
 
 export OPS=$(operating_system)
 function condensed_pwd {
-	if [ $UID -eq 0 ]; then
-		echo "${PWD:t}"
-	else
-		echo "${PWD/#$HOME/~}" | sed 's|~/GitHub/mine/config/NixOS-configs|$NIXCFG|g' | sed 's|~/GitHub/mine/config|$CFG|g' | sed 's|~/GitHub/mine|$GHUBM|g' | sed 's|~/GitHub/others|$GHUBO|g' | sed 's|~/GitHub|$GHUB|g' | sed 's|~/VirtMachines|$VM|g' | sed 's|/arch/home/fusion809/GitHub/mine/config|$ARCHCFG|g' | sed 's|/arch/home/fusion809/GitHub/mine/websites/images|$ARCHIM|g' | sed 's|/arch/home/fusion809/GitHub/mine/websites/fusion809.github.io|$ARCHFGI|g' | sed 's|/arch/home/fusion809/GitHub/mine/websites|$ARCHWEB|g' | sed 's|/arch/home/fusion809/GitHub/mine|$ARCHGBM|g' | sed 's|/arch/home/fusion809/VirtualBox VMs/iso|$ARCHISO|g' | sed 's|/arch/home/fusion809/VirtualBox VMs|$ARCHVM|g' | sed 's|/arch/home/fusion809|$ARCHH|g' | sed 's|/home/fusion809|$HOME|g'
-	fi
+	echo "${PWD/#$HOME/~}" | sed 's|/home/fusion809|~|g' | sed 's|/data/GitHub-nixos|~/GitHub|g' | sed 's|~/GitHub/mine/config/NixOS-configs|$NIXCFG|g' | sed 's|~/GitHub/mine/config|$CFG|g' | sed 's|~/GitHub/mine|$GHUBM|g' | sed 's|~/GitHub/others|$GHUBO|g' | sed 's|~/GitHub|$GHUB|g' | sed 's|~/VirtMachines|$VM|g' | sed 's|/arch/home/fusion809/GitHub/mine/config|$ARCHCFG|g' | sed 's|/arch/home/fusion809/GitHub/mine/websites/images|$ARCHIM|g' | sed 's|/arch/home/fusion809/GitHub/mine/websites/fusion809.github.io|$ARCHFGI|g' | sed 's|/arch/home/fusion809/GitHub/mine/websites|$ARCHWEB|g' | sed 's|/arch/home/fusion809/GitHub/mine|$ARCHGBM|g' | sed 's|/arch/home/fusion809/VirtualBox VMs/iso|$ARCHISO|g' | sed 's|/arch/home/fusion809/VirtualBox VMs|$ARCHVM|g' | sed 's|/arch/home/fusion809|$ARCHH|g' | sed 's|/home/fusion809|$HOME|g'
 }
 
 PROMPT_PREFIX='%{$fg_bold[yellow]%}[%D{%H:%M:%S %d/%m/%y}] $(user) %{$fg_bold[cyan]%}${OPS} %{$fg_bold[blue]%}$(condensed_pwd)$(git_prompt_info)'
