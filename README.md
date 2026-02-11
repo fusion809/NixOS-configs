330610176285213942ce7c14aa17d01bdbb204ed
fusion809 <brentonhorne77@gmail.com>
2026-02-11 14:11:44 +1000

    Fixing Arch part of operating_system filter

--- README.md
+++ README.md
@@ -8,8 +8,8 @@ These are my [NixOS 25.11](https://nixos.org) configuration files for my [MS-7B9
 <!-- STATS START -->
 | Language | Lines | Lines % | Complexity | Complexity % |
 | :--- | :--- | :--- | :--- | :--- |
-| Shell | 3478 | 100.00% | 425 | 100.00% |
-| **Total** | **3478** | **100.00%** | **425** | **100.00%** |
+| Shell | 3475 | 100.00% | 425 | 100.00% |
+| **Total** | **3475** | **100.00%** | **425** | **100.00%** |
 <!-- STATS END -->
 
 # Shell profile
--- shell/hnixos.zsh
+++ shell/hnixos.zsh
@@ -4,7 +4,7 @@ function prompt_char {
 }
 
 function operating_system {
-	printf "$(grep "PRETTY_NAME" < /etc/os-release | cut -d '=' -f 2 | head -n 1 | cut -d '"' -f 2 | sed 's/ (.*)//g' | sed 's/ Linux//g' | sed 's/NixOS.*//' | sed 's/Linux From Scratch//g')" | sed 's/Gentoo Linux//' | sed 's/Arch Linux//'
+	printf "$(grep "PRETTY_NAME" < /etc/os-release | cut -d '=' -f 2 | head -n 1 | cut -d '"' -f 2 | sed 's/ (.*)//g' | sed 's/ Linux//g' | sed 's/NixOS.*//' | sed 's/Linux From Scratch//g')" | sed 's/Gentoo Linux//' | sed 's/Arch.*//'
 }
 
 function user {
