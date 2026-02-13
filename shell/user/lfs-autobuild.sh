ffafe3f19ecd2a47b3c637de48f528b7576f14a0
fusion809 <brentonhorne77@gmail.com>
2026-02-13 21:51:00 +1000

    Some fixes for Vim

--- shell/user/lfs-autobuild.sh
+++ shell/user/lfs-autobuild.sh
@@ -401,7 +401,8 @@ fi
 
 if [[ "$PACKAGE" == "vim" ]]; then
     log "Removing /usr/bin/vi symlink creation..."
-    COMMANDS=$(echo "$COMMANDS" | sed '/ln .* \/usr\/bin\/vi/d')
+    COMMANDS=$(echo "$COMMANDS" | sed '/ln .* \/usr\/bin\/vi/d' | sed '/for L in.*do/d' | sed '/done/d' | sed '/ln -sv vim.1.*vi.1/d')
+    COMMANDS=$(echo "$COMMANDS" | sed "\|ln -sv ../vim/vim91/doc /usr/share/doc/vim-$UPSTREAM_VERSION|i rm -rf /usr/share/doc/vim-*")
 fi
 
 if [[ "$DRY_RUN" == "true" ]]; then
