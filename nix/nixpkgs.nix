917410dd50f1381d81e48cd5b9b00a921456dcd6
fusion809 <brentonhorne77@gmail.com>
2026-02-19 18:44:24 +1000

    updates: Updating flake.lock

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -5,13 +5,23 @@
     allowUnfree = true;
     nvidia.acceptLicense = true;
     permittedInsecurePackages = [ "openssl-1.1.1w" "qtwebengine-5.15.19" ];
-    allowUnfreePredicate = pkg:
-      builtins.elem (lib.getName pkg) [ "antigravity" ];
     packageOverrides = pkgs: {
-      unstable = inputs.nixpkgs-unstable.legacyPackages.x86_64-linux;
-      master = inputs.nixpkgs-master.legacyPackages.x86_64-linux;
-      pr = inputs.nixpkgs-pr.legacyPackages.x86_64-linux;
-      oldstable = inputs.nixpkgs-oldstable.legacyPackages.x86_64-linux;
+      unstable = import inputs.nixpkgs-unstable {
+        system = "x86_64-linux";
+        config.allowUnfree = true;
+      };
+      master = import inputs.nixpkgs-master {
+        system = "x86_64-linux";
+        config.allowUnfree = true;
+      };
+      pr = import inputs.nixpkgs-pr {
+        system = "x86_64-linux";
+        config.allowUnfree = true;
+      };
+      oldstable = import inputs.nixpkgs-oldstable {
+        system = "x86_64-linux";
+        config.allowUnfree = true;
+      };
     };
   };
   overlays = import ./overlays.nix { inherit inputs username; };
