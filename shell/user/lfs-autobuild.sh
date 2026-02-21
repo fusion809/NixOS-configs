4ee5fcb58de904c77481545beef0ce0d2a68d0f7
fusion809 <brentonhorne77@gmail.com>
2026-02-21 18:50:56 +1000

    Fixing spidermonkey build by rm tests

--- shell/user/lfs-autobuild.sh
+++ shell/user/lfs-autobuild.sh
@@ -200,7 +200,7 @@ while read -r line; do
 
         # 4. Determine if this block is a test suite or related setup
         # keywords: make/ninja tests, expect scripts, tester user, su to tester, testdir
-        if [[ "$CURRENT_BLOCK" =~ (make[[:space:]]+(check|test|tests)|ninja[[:space:]]+test|spawn[[:space:]]+make|\<expect\>|tester|su[[:space:]]+.*tester|testdir) ]]; then
+        if [[ "$CURRENT_BLOCK" =~ (make.*(check|test|tests|jstest|jit-test)|ninja.*test|spawn.*make|\<expect\>|tester|su.*tester|testdir) ]]; then
             if [[ "$is_critical" == "true" ]]; then
                 # Wrap critical test block in prompt
                 # Note: Remove trailing newline for cleaner injection
