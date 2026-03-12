22c293bcae86f3df430053596c0a00c3264c235f
fusion809 <brentonhorne77@gmail.com>
2026-03-12 22:03:43 +1000

    updates: Updating flake.lock

--- nix/flake.nix
+++ nix/flake.nix
@@ -8,60 +8,6 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     # Hyprland
-    hy3 = {
-      url = "github:outfoxxed/hy3";
-      #refs/tags/hl0.52.0"; # where {version} is the hyprland release version
-      # or "github:outfoxxed/hy3" to follow the development branch.
-      # (you may encounter issues if you dont do the same for hyprland)
-      inputs.hyprland.follows = "hyprland";
-    };
-    # use the github shorthand with the tag; this resolves Git refs more reliably
-    hyprland.url = "github:hyprwm/Hyprland?submodules=1";
-    # where 0.52.1 is the hyprland release version
-    # or "github:hyprwm/Hyprland?submodules=1" to follow the development branch
-    hyprland.inputs.nixpkgs.follows = "nixpkgs";
-
-    hyprland-guiutils.url = "github:hyprwm/hyprland-guiutils";
-    hyprland-guiutils.inputs.nixpkgs.follows = "nixpkgs";
-    hyprlang.url = "github:hyprwm/hyprlang";
-    hyprlang.inputs.nixpkgs.follows = "nixpkgs";
-    hyprtoolkit.url = "github:hyprwm/hyprtoolkit";
-    hyprtoolkit.inputs.nixpkgs.follows = "nixpkgs";
-    hyprutils.url = "github:hyprwm/hyprutils";
-    hyprutils.inputs.nixpkgs.follows = "nixpkgs";
-    aquamarine.url = "github:hyprwm/aquamarine";
-    aquamarine.inputs.nixpkgs.follows = "nixpkgs";
-    hyprwayland-scanner.url = "github:hyprwm/hyprwayland-scanner";
-    hyprwayland-scanner.inputs.nixpkgs.follows = "nixpkgs";
-    hyprgraphics.url = "github:hyprwm/hyprgraphics";
-    hyprgraphics.inputs.nixpkgs.follows = "nixpkgs";
-
-    hyprland.inputs.hyprland-guiutils.follows = "hyprland-guiutils";
-    hyprland-guiutils.inputs.hyprtoolkit.follows = "hyprtoolkit";
-
-    hyprland.inputs.hyprlang.follows = "hyprlang";
-    hyprland-guiutils.inputs.hyprlang.follows = "hyprlang";
-    hyprtoolkit.inputs.hyprlang.follows = "hyprlang";
-
-    hyprland.inputs.hyprutils.follows = "hyprutils";
-    hyprland-guiutils.inputs.hyprutils.follows = "hyprutils";
-    hyprtoolkit.inputs.hyprutils.follows = "hyprutils";
-    hyprlang.inputs.hyprutils.follows = "hyprutils";
-
-    hyprland.inputs.aquamarine.follows = "aquamarine";
-    hyprland-guiutils.inputs.aquamarine.follows = "aquamarine";
-    hyprtoolkit.inputs.aquamarine.follows = "aquamarine";
-
-    hyprland.inputs.hyprwayland-scanner.follows = "hyprwayland-scanner";
-    hyprland-guiutils.inputs.hyprwayland-scanner.follows =
-      "hyprwayland-scanner";
-    hyprtoolkit.inputs.hyprwayland-scanner.follows = "hyprwayland-scanner";
-    aquamarine.inputs.hyprwayland-scanner.follows = "hyprwayland-scanner";
-
-    hyprland.inputs.hyprgraphics.follows = "hyprgraphics";
-    hyprland-guiutils.inputs.hyprgraphics.follows = "hyprgraphics";
-    hyprtoolkit.inputs.hyprgraphics.follows = "hyprgraphics";
-
     # nixpkgs
     nixpkgs-oldstable.url =
       "git+https://github.com/NixOS/nixpkgs.git?ref=nixos-25.05";
@@ -82,18 +28,12 @@
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, hyprland, hy3, nixvim, ... }@inputs:
+  outputs = { self, nixpkgs, home-manager, nixvim, ... }@inputs:
     let username = "fusion809";
     in {
-      # expose the hy3 package from the hy3 input so it can be built directly
-      packages.x86_64-linux.hy3 = hy3.packages.x86_64-linux.hy3;
-      packages.x86_64-linux.hyprland-guiutils =
-        inputs.hyprland-guiutils.packages.x86_64-linux.hyprland-guiutils;
-      packages.x86_64-linux.hyprtoolkit =
-        inputs.hyprtoolkit.packages.x86_64-linux.hyprtoolkit;
-
       # also set the defaultPackage for this system to hy3 for convenience
-      defaultPackage.x86_64-linux = hy3.packages.x86_64-linux.hy3;
+      defaultPackage.x86_64-linux =
+        inputs.nixpkgs-unstable.legacyPackages.x86_64-linux.hyprlandPlugins.hy3;
 
       nixosConfigurations."nixos" = nixpkgs.lib.nixosSystem {
         specialArgs = { inherit inputs username; };
@@ -114,15 +54,10 @@
           }
           {
             nixpkgs.overlays = [
-              inputs.hyprland.overlays.glaze
-              inputs.hyprland.overlays.hyprland-packages
-              inputs.hyprland-guiutils.overlays.hyprland-guiutils-with-deps
               (final: prev: {
-                hyprland = prev.hyprland.override {
-                  hyprland-guiutils = final.hyprland-guiutils;
-                };
+                hyprland = final.unstable.hyprland;
                 hyprlandPlugins = prev.hyprlandPlugins // {
-                  hy3 = hy3.packages.${prev.stdenv.hostPlatform.system}.hy3;
+                  hy3 = final.unstable.hyprlandPlugins.hy3;
                 };
               })
             ];
--- shell/user/15-hyprland.sh
+++ shell/user/15-hyprland.sh
@@ -1,36 +1,4 @@
 # Update flake.nix
 export FLAKE_FILE="$NIXCFG/nix/flake.nix"
-# Fetch latest Hyprland release
-export HYPR_TAG=$(wget -cqO- https://github.com/hyprwm/Hyprland/tags | grep 'tag/v' | head -n 1 | cut -d '"' -f 6 | cut -d '/' -f 6)
-if [[ $HYPR_TAG != "null" ]]; then
-    echo $HYPR_TAG > $HOME/.cache/hyprland_latest
-elif [[ -f $HOME/.cache/hyprland_latest ]]; then
-    export HYPR_TAG=$(cat $HOME/.cache/hyprland_latest)
-fi
-export HYPR_INST=$(cat $FLAKE_FILE | grep "Hyprland?" | cut -d '"' -f 2 | cut -d '/' -f 7)
-# Fetch latest hy3 release
-export HY3_TAG=$(wget -cqO- https://github.com/outfoxxed/hy3/tags | grep 'tag/hl' | head -n 1 | cut -d '"' -f 6 | cut -d '/' -f 6)
-if [[ $HY3_TAG != "null" ]]; then
-    echo $HY3_TAG > $HOME/.cache/hy3_latest
-elif [[ -f $HOME/.cache/hy3_latest ]]; then
-    export HY3_TAG=$(cat $HOME/.cache/hy3_latest)
-fi
-export HY3_INST=$(cat $FLAKE_FILE | grep "hy3?" | cut -d '"' -f 2 | cut -d '/' -f 7)
-# if ( [[ -n $HY3_TAG ]] && [[ $HY3_TAG != $HY3_INST ]] && [[ -n $HY3_INST ]] && [[ $HY3_TAG != "null" ]] ); then
-#     echo "New version of hy3, $HY3_TAG, is out. Run hyprupdate to apply this update."
-# fi
-# if ( [[ $HYPR_INST != $HYPR_TAG ]] && [[ -n $HYPR_TAG ]] && [[ $HYPR_TAG != "null" ]] ); then
-#     echo "New version of Hyprland, $HYPR_TAG, is out. Run hyprupdate to apply this update."
-# fi
-
-# function hyprupdate {
-#     if ( ( [[ -n $HY3_TAG ]] && [[ $HY3_TAG != $HY3_INST ]] && [[ -n $HY3_INST ]] && [[ $HY3_TAG != "null" ]] ) || ( [[ $HYPR_INST != $HYPR_TAG ]] && [[ -n $HYPR_TAG ]] && [[ $HYPR_TAG != "null" ]] ) ) ; then
-#         # Update hy3 url
-#         sed -i \
-#         -e "s|https://github.com/outfoxxed/hy3?ref=refs/tags/$HY3_INST|https://github.com/outfoxxed/hy3?ref=refs/tags/$HY3_TAG|" \
-#         -e "s|https://github.com/hyprwm/Hyprland?submodules=1&ref=refs/tags/$HYPR_INST|https://github.com/hyprwm/Hyprland?submodules=1&ref=refs/tags/$HYPR_TAG|" "$FLAKE_FILE"
-#         echo "Updated flake.nix to use Hyprland $HYPR_TAG and hy3 $HY3_TAG"
-#         nixrsu
-#         sed -i -e "s|hyprupdate|nixfrb|g" $NIXCFG/shell/hyprland/update_func
-#     fi
-# }
+# Update flake.nix
+export FLAKE_FILE="$NIXCFG/nix/flake.nix"
