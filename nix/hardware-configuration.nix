405bc58b9f223f6076dc35391b58fc5ea6d07462
fusion809 <brentonhorne77@gmail.com>
2026-04-27 08:04:35 +1000

    updates: Updating flake.lock

--- nix/hardware-configuration.nix
+++ nix/hardware-configuration.nix
@@ -43,6 +43,7 @@
     device = "/swapfile";
     size = 64 * 1024;
   }];
+  zramSwap.enable = true;
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
