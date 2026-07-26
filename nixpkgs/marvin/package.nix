3676fbf4b958c31c60f3934c2550d25bfe4421e2
fusion809 <brentonhorne77@gmail.com>
2026-07-26 16:46:21 +1000

    updates: Updating flake.lock

--- nixpkgs/marvin/package.nix
+++ nixpkgs/marvin/package.nix
@@ -4,13 +4,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "marvin";
-  version = "25.5.0";
+  version = "26.1.1";
 
   src = fetchurl {
     name = "marvin-${finalAttrs.version}.deb";
     url =
       "http://dl.chemaxon.com/marvin/${finalAttrs.version}/marvin_linux_${finalAttrs.version}.deb";
-    hash = "sha256-+fTO6cEJL4QRFpLQ9CXZFt7Jg3otR3ZMWN5vH+3QXmA=";
+    hash = "sha256-s52e/J6ohtUv5mSfEXaOGeC1FKkjwgFUH7wLSGqZRFM=";
   };
 
   nativeBuildInputs = [ dpkg makeWrapper ];
