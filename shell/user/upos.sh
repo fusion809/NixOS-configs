760409d164946c8def6fe2a889e11eac75ffb731
fusion809 <brentonhorne77@gmail.com>
2026-07-27 22:56:01 +1000

    Improving checks that blfs_ver isn't blank

--- shell/user/upos.sh
+++ shell/user/upos.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 lfs_ver=$(wget -cqO- https://www.linuxfromscratch.org/lfs/view/systemd/index.html | grep 'Version' | sed 's/^\s*//g' | cut -d ' ' -f 2 | sed 's/-systemd//g')
 blfs_ver=$(wget -cqO- https://www.linuxfromscratch.org/blfs/view/systemd/ | grep "id=\"" | head -n 1 | cut -d '"' -f 4 | sed 's/blfs-//g')
-if echo $upver | grep '^r'; then
+if echo $lfs_ver | grep '^r'; then
     sudo sed -i -E "s|r[0-9]{2,}\.[0-9]-[0-9]+|$lfs_ver|g" /etc/os-release /etc/lfs-release /etc/lsb-release
+fi
+
+if echo $blfs_ver | grep '^r'; then
     sudo sed -i -E "s|r[0-9]{2,}\.[0-9]-[0-9]+|$blfs_ver|g" /etc/blfs-release
 fi
