8e0fe5e1dc00d8908fa675684201fabd44a0de49
fusion809 <brentonhorne77@gmail.com>
2026-07-29 21:19:39 +1000

    Fixing walloptim

--- shell/user/20-misc.sh
+++ shell/user/20-misc.sh
@@ -223,8 +223,8 @@ function send_walk {
 function walloptim {
 	find ~/Pictures/Wallpapers -type f \( -iname '*.jpg' -o -iname '*.jpeg' \) \
 		-exec jpegoptim --strip-all --all-progressive {} +
-	sudo find /usr/share/wallpapers -type f \( -iname '*.jpg' -o -iname '*.jpeg' \) \
+	sudo find /arch/usr/share/wallpapers -type f \( -iname '*.jpg' -o -iname '*.jpeg' \) \
 		-exec sudo jpegoptim --strip-all --all-progressive {} +
-	sudo find /usr/share/backgrounds -type f \( -iname '*.jpg' -o -iname '*.jpeg' \) \
+	sudo find /arch/usr/share/backgrounds -type f \( -iname '*.jpg' -o -iname '*.jpeg' \) \
 		-exec sudo jpegoptim --strip-all --all-progressive {} +
 }
