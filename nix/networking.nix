76e23b96b4de42045bc2bb8981144c523aaece6e
fusion809 <brentonhorne77@gmail.com>
2026-08-24 20:25:41 +1000

    updates: Updating flake.lock

--- nix/networking.nix
+++ nix/networking.nix
@@ -4,4 +4,10 @@
   hostName = "nixos"; # Define your hostname.
   # Enable networking
   networkmanager.enable = true;
+
+  # WayVNC ports (5900 for HDMI-A-1, 5901 for DVI-D-1)
+  firewall = {
+    enable = true;
+    allowedTCPPorts = [ 5900 5901 ];
+  };
 }
--- nix/packages.nix
+++ nix/packages.nix
@@ -14,6 +14,7 @@ with pkgs;
   nixfmt # Needed for Nix IDE extension of vscode/antigravity
   opencode
   pinta
+  unstable.wayvnc
   vscode
   inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.default
   ###############################################################
